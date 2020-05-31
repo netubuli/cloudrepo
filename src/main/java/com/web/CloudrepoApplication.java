7bab936d11cb0528668c54323d941b841f240aca
netubuli <netubuli@gmail.com>
2020-05-31 14:31:52 +0300

    edited main class

--- src/main/java/com/web/CloudrepoApplication.java
+++ src/main/java/com/web/CloudrepoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CloudrepoApplication {
+	//main class for the entire project
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudrepoApplication.class, args);
